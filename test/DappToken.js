cb0dded13d506686e6f95c42fd843a87f43b6787
Vin_04 <vinayakborhade@gmail.com>
2018-12-25 16:28:09 +0530

    2. Transfer tokens

--- test/DappToken.js
+++ test/DappToken.js
@@ -13,12 +13,89 @@ contract('DappToken', function(accounts) {
 });*/
 
 contract('DappToken', function(accounts) {
+	var tokenInstance;
+
+	it('initializes contract with correct values', function() {
+		return DappToken.deployed().then(function(instance) {
+			tokenInstance=instance;
+			tokenInstance.name().then(function(name) {
+				assert.equal(name, 'DApp Token', 'has the correct name');
+			});
+			tokenInstance.symbol().then(function(symbol) {
+				assert.equal(symbol, 'DAPP', 'has the correct symbol');
+			});
+			tokenInstance.standard().then(function(standard) {
+				assert.equal(standard, 'DApp Token v1.0', 'has the correct standard');
+			});
+		});
+	});
+
 	it('sets the total supply upon deployment', function() {
 		return DappToken.deployed().then(function(instance) {
 			tokenInstance=instance;
 			tokenInstance.totalSupply().then(function(totalSupplyValue) {
 				assert.equal(totalSupplyValue.toNumber(), 1000000, 'sets the total supply to 10^6 ');
 			});
+			tokenInstance.balanceOf(accounts[0]).then(function(adminBalance) {
+				assert.equal(adminBalance.toNumber(), 1000000, 'allocates initial supply to admin');
+			});
 		});
-	})
+	});
+
+	/*
+	it('transfers token ownership', function() {
+		return DappToken.deployed().then(function(instance) {
+			tokenInstance=instance;
+			tokenInstance.transfer.call(accounts[1], 99999999999999999)
+			.then(assert.fail).catch(function(error) {
+				assert(error.message.indexOf('revert') >= 0, 'error message must contain revert');
+			}).then(function(success) {
+				assert.equal(success, true, 'it returns ture');
+			});
+
+			tokenInstance.transfer(accounts[1], 250000, {from: accounts[0]})
+			.then(function(receipt) {
+				assert.equal(receipt.logs.length, 1, 'triggers one event');
+				assert.equal(receipt.logs[0].event, 'Transfer', 'should be the "Transfer" event');
+				assert.equal(receipt.logs[0].args._from, accounts[0], 'logs a/c the tokens are transferred from');
+				assert.equal(receipt.logs[0].args._to, accounts[1], 'logs a/c the tokens are transferred to');
+				assert.equal(receipt.logs[0].args._value, 250000, 'logs transfer amount');
+			});
+			tokenInstance.balanceOf(accounts[1]).then(function(balance) {
+				assert.equal(balance.toNumber(), 250000, 'adds the amount to receiving account');
+			});
+			tokenInstance.balanceOf(accounts[0]).then(function(balance) {
+				assert.equal(balance.toNumber(), 750000, 'deducts the amount from sending account');
+			});
+		});
+	});*/
+	
+	
+	it('transfers token ownership', function() {
+    return DappToken.deployed().then(function(instance) {
+      tokenInstance = instance;
+      // Test `require` statement first by transferring something larger than the sender's balance
+      return tokenInstance.transfer.call(accounts[1], 99999999999999999999999);
+    }).then(assert.fail).catch(function(error) {
+      assert(error.message.indexOf('revert') >= 0, 'error message must contain revert');
+      return tokenInstance.transfer.call(accounts[1], 250000, { from: accounts[0] });
+    }).then(function(success) {
+      assert.equal(success, true, 'it returns true');
+      return tokenInstance.transfer(accounts[1], 250000, { from: accounts[0] });
+    }).then(function(receipt) {
+      assert.equal(receipt.logs.length, 1, 'triggers one event');
+      assert.equal(receipt.logs[0].event, 'Transfer', 'should be the "Transfer" event');
+      assert.equal(receipt.logs[0].args._from, accounts[0], 'logs the account the tokens are transferred from');
+      assert.equal(receipt.logs[0].args._to, accounts[1], 'logs the account the tokens are transferred to');
+      assert.equal(receipt.logs[0].args._value, 250000, 'logs the transfer amount');
+      return tokenInstance.balanceOf(accounts[1]);
+    }).then(function(balance) {
+      assert.equal(balance.toNumber(), 250000, 'adds the amount to the receiving account');
+      return tokenInstance.balanceOf(accounts[0]);
+    }).then(function(balance) {
+      assert.equal(balance.toNumber(), 750000, 'deducts the amount from the sending account');
+    });
+  });
+	
+
 });
