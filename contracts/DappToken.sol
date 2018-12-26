445aa66ad2a3752c3fda872538fb4821df4a8e21
Vin_04 <vinayakborhade@gmail.com>
2018-12-26 11:35:06 +0530

    3. Delegated Transfer

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -8,7 +8,10 @@ contract DappToken {
 
 	event Transfer(address indexed _from, address indexed _to, uint256 _value);
 
+	event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+
 	mapping(address => uint256) public balanceOf;
+	mapping(address => mapping(address => uint256)) public allowance;
 
 	function DappToken(uint256 _initialSupply) public {
 		balanceOf[msg.sender]=_initialSupply;
@@ -20,6 +23,12 @@ contract DappToken {
 		balanceOf[msg.sender]-=_value;
 		balanceOf[_to]+=_value;
 		Transfer(msg.sender, _to, _value);
+		return true;
+	}
+
+	function approve(address _spender, uint256 _value) public returns (bool success) {
+		allowance[msg.sender][_spender]=_value;
+		Approval(msg.sender, _spender, _value);
 		return true;
 	}
 
